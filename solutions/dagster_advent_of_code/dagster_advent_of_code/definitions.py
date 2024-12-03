1bc38fc000fa376b80faa80aa4f2d4467c1bfcba
ex00 <13835761+ex00@users.noreply.github.com>
2024-12-03 23:18:42 +0000

    day 3 solution

--- solutions/dagster_advent_of_code/dagster_advent_of_code/definitions.py
+++ solutions/dagster_advent_of_code/dagster_advent_of_code/definitions.py
@@ -1,12 +1,11 @@
 from dagster import Definitions, load_assets_from_modules
 
-from dagster_advent_of_code import assets_quick_start, assets_day2, schedules_day2 # noqa: TID252
+from dagster_advent_of_code import assets_quick_start, assets_day3 # noqa: TID252
     
 
-all_assets = load_assets_from_modules([assets_quick_start, assets_day2])
+all_assets = load_assets_from_modules([assets_quick_start, assets_day3])
 
 
 defs = Definitions(
     assets=all_assets,
-    schedules=[schedules_day2.day2_schedule_definition, schedules_day2.day2_schedule_definition_second]
 )
