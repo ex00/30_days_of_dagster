ebfdd5a3c3d08b72d3ef4f0260140f87ad993e76
ex00 <13835761+ex00@users.noreply.github.com>
2024-12-08 20:18:58 +0000

    day 8 solution

--- solutions/dagster_advent_of_code/dagster_advent_of_code/definitions.py
+++ solutions/dagster_advent_of_code/dagster_advent_of_code/definitions.py
@@ -1,11 +1,12 @@
 from dagster import Definitions, load_assets_from_modules
 
-from dagster_advent_of_code import assets_quick_start, assets_day7 # noqa: TID252
+from dagster_advent_of_code import assets_quick_start, assets_day8 # noqa: TID252
     
 
-all_assets = load_assets_from_modules([assets_quick_start, assets_day7])
+all_assets = load_assets_from_modules([assets_quick_start, assets_day8])
 
 
 defs = Definitions(
-    assets=all_assets
+    assets=all_assets,
+    sensors=[assets_day8.run_asserts_on_top_changed_data]
 )
