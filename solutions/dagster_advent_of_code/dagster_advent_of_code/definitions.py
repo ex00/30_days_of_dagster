78dc8ffc8c408c7665de3d6df3e8048499b0e4b6
ex00 <13835761+ex00@users.noreply.github.com>
2024-12-11 21:26:08 +0000

    day 11 solution

--- solutions/dagster_advent_of_code/dagster_advent_of_code/definitions.py
+++ solutions/dagster_advent_of_code/dagster_advent_of_code/definitions.py
@@ -1,16 +1,17 @@
 from dagster import Definitions, load_assets_from_modules
 
-from dagster_advent_of_code import assets_quick_start, assets_day10 # noqa: TID252
+from dagster_advent_of_code import assets_quick_start, assets_day11 # noqa: TID252
     
 
-all_assets = load_assets_from_modules([assets_quick_start, assets_day10])
+all_assets = load_assets_from_modules([assets_quick_start, assets_day11])
 
 
 defs = Definitions(
     assets=all_assets,
-    sensors=[assets_day10.run_asserts_on_top_changed_data],
+    sensors=[assets_day11.run_asserts_on_top_changed_data],
+    asset_checks=[assets_day11.check_read_data],
     resources={
-        "raw_data": assets_day10.DataToFileResource(path=assets_day10.MyAssetConfig().raw_file_path),
-        "bronze_data": assets_day10.DataToFileResource(path=assets_day10.MyAssetConfig().bronze_file_path),
+        "raw_data": assets_day11.DataToFileResource(path=assets_day11.MyAssetConfig().raw_file_path),
+        "bronze_data": assets_day11.DataToFileResource(path=assets_day11.MyAssetConfig().bronze_file_path),
     }
 )
